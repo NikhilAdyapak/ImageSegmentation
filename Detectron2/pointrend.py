7b3723618a7452c1c8168a91c54b4d0620619e7f
NikhilAdyapak <56066146+NikhilAdyapak@users.noreply.github.com>
2022-07-20 15:39:25 +0530

    update

--- Detectron2/pointrend.py
+++
@@ -1,6 +0,0 @@
-import pixellib
-from pixellib.torchbackend.instance import instanceSegmentation
-
-ins = instanceSegmentation()
-ins.load_model("pointrend_resnet50.pkl")
-ins.segmentImage("cycle.jpg", show_bboxes=True, output_image_name="output_point.jpg")
